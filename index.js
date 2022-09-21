066a0a1ce8266ca59fafcca94ba5304cce1a2066
Bartholomew Hauptly <bhauptly11@gmail.com>
2022-09-21 08:55:09 -0500

    commit tree ed35f5e241db6489c0758213ec213e6451ad4357

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    changeText('This is really cool!')
+})
+    console.log("Before DOM loads");
+
+function changeText(text) {
+    let p = document.querySelector('p')
+    p.textContent = text
+}
